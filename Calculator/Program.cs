35fbcd00b54a89b995eaf07708007b883b724134
Vinícius <viniciuscarmo.contato@gmail.com>
2023-10-31 20:45:20 -0300

    Finalizando o projeto básico da cálculadora, foi implementado uma função menu com interação com usuário.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,8 +10,49 @@ namespace Calculator
         {
         static void Main(string[] args)
             {
-
+            Console.WriteLine("Bem vindo a calculadora básica de dois operadores!");
+            Console.ReadKey();
+            Menu();
             }
+
+        static void Menu()
+            {
+            Console.Clear();
+
+            Console.WriteLine("Qual operação você deseja executar:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine("");
+
+            Console.Write("Selecione a sua opção: ");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+                {
+                case 1:
+                    Soma();
+                    break;
+                case 2:
+                    Subtracao();
+                    break;
+                case 3:
+                    Divisao();
+                    break;
+                case 4:
+                    Multiplicacao();
+                    break;
+                case 5:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Menu();
+                    break;
+                }
+            }
+
         static void Soma()
             {
             Console.Clear();
